28b6406dc48ba8a15f294c65fafdf72fe09c8059
Andrea Parodi <andrea@parro.it>
2021-03-08 11:22:58 +0100

    generic

---
+++ genericfs/fs.go
@@ -0,0 +1,60 @@
+package sshfs
+
+import (
+	"io/fs"
+
+	"github.com/parro-it/vs/writefs"
+)
+
+// FS ...
+type FS struct{}
+
+var (
+	_ fs.StatFS        = FS{}
+	_ fs.ReadFileFS    = FS{}
+	_ fs.SubFS         = FS{}
+	_ writefs.WriteFS  = FS{}
+	_ writefs.RemoveFS = FS{}
+	_ writefs.MkDirFS  = FS{}
+	_ fs.ReadDirFS     = FS{}
+)
+
+// MkDir implements writefs.MkDirFS
+func (fsys FS) MkDir(name string, perm fs.FileMode) error {
+	return nil
+}
+
+// Remove implements writefs.RemoveFS
+func (fsys FS) Remove(name string, perm fs.FileMode) error {
+	return nil
+}
+
+// OpenFile implements writefs.WriteFS
+func (fsys FS) OpenFile(name string, flag int, perm fs.FileMode) (writefs.FileWriter, error) {
+	return nil, nil
+}
+
+// Stat implements fs.StatFS
+func (fsys FS) Stat(name string) (fs.FileInfo, error) {
+	return nil, nil
+}
+
+// ReadFile implements fs.ReadFileFS
+func (fsys FS) ReadFile(name string) ([]byte, error) {
+	return nil, nil
+}
+
+// Sub implements fs.SubFS
+func (fsys FS) Sub(dir string) (fs.FS, error) {
+	return nil, nil
+}
+
+// Open implements fs.FS
+func (fsys FS) Open(name string) (fs.File, error) {
+	return nil, nil
+}
+
+// ReadDir implements fs.ReadDirFS
+func (fsys FS) ReadDir(name string) ([]fs.DirEntry, error) {
+	return nil, nil
+}
---
+++ genericfs/fs_test.go
@@ -0,0 +1,15 @@
+package sshfs
+
+import (
+	"testing"
+	"testing/fstest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSSHFS(t *testing.T) {
+	t.Run("pass TestFS", func(t *testing.T) {
+		err := fstest.TestFS(nil, "dir1/file2", "file1")
+		assert.NoError(t, err)
+	})
+}
